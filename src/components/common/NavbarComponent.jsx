ca38d89fbbe7e5f8b92c8b880f9d20b82aaf9918
shrey802 <shreyashpingle752@gmail.com>
2023-07-25 19:53:36 +0530

    homepage responsive

--- src/components/common/NavbarComponent.jsx
+++ src/components/common/NavbarComponent.jsx
@@ -1,67 +1,102 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUniversity } from 'react-icons/fa';
 import { AiFillPhone, AiFillCodeSandboxSquare } from 'react-icons/ai';
 import { SiAboutdotme } from 'react-icons/si';
 import { GiPlagueDoctorProfile } from 'react-icons/gi';
-import '../common/navbar.css';
+import { Link, useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import { Logout } from '../../api/Authapi';
-import { useNavigate } from 'react-router-dom';
 
 export default function NavbarComponent() {
-  // BASIC NAVBAR 
   const navigate = useNavigate();
-// NAVIGATION TO LOGIN
+  const [isNavOpen, setNavOpen] = useState(false); // State to manage Navbar collapse
+
   function navigation() {
     Logout();
     navigate('/login');
   }
-// TO CONTRIBUTE
+
   function contriNavigate() {
     navigate('/contribute');
+    closeNavbar();
   }
-// TO ABOUT
+
   function about() {
     navigate('/about');
+    closeNavbar();
   }
-// TO CONTACT
+
   function handleContact() {
     navigate('/contact');
+    closeNavbar();
   }
-// TO PROFILE
+
   function handleProfile() {
     navigate('/profile');
+    closeNavbar();
   }
-// TO FAQs
-function faq() {
-  navigate('/faqs');
-}
 
-// NAVBAR OF PRODUCTS PAGE
+  function faq() {
+    navigate('/faqs');
+    closeNavbar();
+  }
+
+  function toggleNavbar() {
+    setNavOpen(!isNavOpen); // Toggle the state when the hamburger menu is clicked
+  }
+
+  function closeNavbar() {
+    setNavOpen(false); // Close the Navbar when a link is clicked
+  }
+
   return (
     <div className='wholedirtynavbar'>
-      <nav className='navbarhaiye'>
-        <h1 className='headingofnavbar'>
+      <nav className='navbar navbar-expand-md navbar-light bg-light navbarhaiye'>
+        <Link to='/' className='navbar-brand headingofnavbar'>
           UnixChange <FaUniversity size={25} />
-        </h1>
-        <div className='navbar-links'>
-          <p className='navbar-link' onClick={handleContact}>
-            <AiFillPhone size={25} /> Contact
-          </p>
-          <p className='navbar-link' onClick={about}>
-            <SiAboutdotme size={25} /> About
-          </p>
-          <p className='navbar-link' onClick={contriNavigate}>
-            Wanna Contribute? <AiFillCodeSandboxSquare size={25} />
-          </p>
-          <p className='navbar-link' onClick={handleProfile}>
-            Your Profile <GiPlagueDoctorProfile size={25} />
-          </p>
-          <p className='navbar-link' onClick={faq}>
-            Support
-          </p>
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          onClick={toggleNavbar} // Call the toggleNavbar function when the button is clicked
+          aria-controls='navbarCollapse'
+          aria-expanded={isNavOpen}
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+
+        <div className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`} id='navbarCollapse'>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item'>
+              <Link to='/contact' className='nav-link' onClick={handleContact}>
+                <AiFillPhone size={25} /> Contact
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/about' className='nav-link' onClick={about}>
+                <SiAboutdotme size={25} /> About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contribute' className='nav-link' onClick={contriNavigate}>
+                Wanna Contribute? <AiFillCodeSandboxSquare size={25} />
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/profile' className='nav-link' onClick={handleProfile}>
+                Your Profile <GiPlagueDoctorProfile size={25} />
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/faqs' className='nav-link' onClick={faq}>
+                Support
+              </Link>
+            </li>
+          </ul>
         </div>
-        <button className='logoutofnavbar' onClick={navigation}>
+        <button className='btn btn-danger logoutofnavbar' onClick={navigation}>
           Log Out
         </button>
       </nav>
